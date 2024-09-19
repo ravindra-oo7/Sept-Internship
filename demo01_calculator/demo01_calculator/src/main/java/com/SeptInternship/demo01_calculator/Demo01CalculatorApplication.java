c447a0f8a630315611867def754d0f89633903a5
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-09-19 11:02:33 +0530

    New Practice Project File Added . This Project is Practice Project which is done for Demo01 submission for HulkHire Internship

---
+++ demo01_calculator/demo01_calculator/src/main/java/com/SeptInternship/demo01_calculator/Demo01CalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.SeptInternship.demo01_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo01CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo01CalculatorApplication.class, args);
+	}
+
+}
---
+++ demo01_calculator/demo01_calculator/src/test/java/com/SeptInternship/demo01_calculator/Demo01CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SeptInternship.demo01_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo01CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
